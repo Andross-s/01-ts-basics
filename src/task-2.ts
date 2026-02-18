5deb00dc1311d8835bc100cb3fe156f6882d9e4e
Andrij Skoropad <andross_s@ukr.net>
2026-02-18 11:03:54 +0200

    fix hw task

--- src/task-2.ts
+++ src/task-2.ts
@@ -4,12 +4,10 @@ interface Product {
   description?: string;
 }
 
-const product: Product[] = [
-  {
-    id: 1,
-    title: "Tablet",
-    description: "Compact and fast",
-  },
-];
+const product: Product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
 
 console.log(`Product: ${JSON.stringify(product)}`);
